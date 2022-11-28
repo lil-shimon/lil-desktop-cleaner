b368995334982c0a546a98a6b9d5c8a473595e3c
Kenta Shimosawa <shimon101018@gmail.com>
2022-11-28 23:59:59 +0900

    clean.sh

--- clean.sh
+++ clean.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-DIR="backups"
+DIR="backps"
 
 cd ~/desktop
-if [ -d $DIR ];then
-  echo "Directory exists"
+if [ ! -d $DIR ];then
+  echo "バックアップディレクトリが存在しません"
+  mkdir $DIR
+  if [ -d $DIR ]; then
+    echo "バックアップディレクトリを作成しました"
+  else
+    echo "バックアップディレクトリの作成に失敗しました"
+  fi
 fi
